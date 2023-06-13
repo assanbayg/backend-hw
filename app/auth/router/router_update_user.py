2e22891c0a9e1a51dc01e2d86c61e53a37ec158b
Gauhar Assanbay <asanbayg@gmail.com>
2023-06-13 06:43:58 +0600

    Add update user

---
+++ app/auth/router/router_update_user.py
@@ -0,0 +1,48 @@
+from typing import Any, Optional
+
+from fastapi import Depends, HTTPException, status
+from pydantic import Field, BaseModel
+
+from app.utils import AppModel
+
+from ..adapters.jwt_service import JWTData
+from ..service import Service, get_service
+from . import router
+from .dependencies import parse_jwt_user_data
+
+
+class UpdateUserRequest(BaseModel):
+    phone: str
+    name: str
+    city: str
+
+
+class UpdateUserResponse(AppModel):
+    phone: str
+    name: str
+    city: str
+
+
+@router.patch(
+    "/users/me",
+    responses={
+        status.HTTP_200_OK: {"model": UpdateUserResponse},
+        status.HTTP_404_NOT_FOUND: {"description": "User not found"},
+    },
+)
+def update_user_by_id(
+    input: UpdateUserRequest,
+    jwt_data: JWTData = Depends(parse_jwt_user_data),
+    svc: Service = Depends(get_service),
+) -> dict[str, str]:
+    user_id = jwt_data.user_id
+
+    updated_user = svc.repository.update_user_by_id(
+        user_id, input.dict(exclude_unset=True)
+    )
+
+    return UpdateUserResponse(
+        phone=updated_user.phone,
+        name=updated_user.name,
+        city=updated_user.city,
+    )
